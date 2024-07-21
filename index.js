b4d15d1b7087ebc5541cb15585c3024db39db2a3
RAYMONDSANG <raysang98@gmail.com>
2024-07-21 17:06:05 +0300

    final commit

--- index.js
+++ index.js
@@ -25,24 +25,50 @@ const nestedMuppet = {
   nestedPartner: 'Miss Piggy'
 };
 
-// Strings
 
-// 1. Use destructuring to assign appropriate variables based on the sounds animals make.
+const [moo, neigh, baa, oink, cluck] = farmAnimals.split(' ');
+console.log(moo, neigh, baa, oink, cluck);
 
-// 2. Bolt the horse wandered off, so just give us four animals, and let's name them bessie, dolly, babe, and little.
 
-// 3. Little the chicken had to go back to the coop, so now we're left with three. Let's use color variables of blackAndWhite, black, and pink.
 
-// Arrays
 
-// 4. Use destructuring to assign appropriate variables using the color names.
+const [bessie, , dolly, babe, little] = farmAnimals.split(' ');
+console.log(bessie, dolly, babe, little);
 
-// 5. Some people have a really hard time picking out indigo, so let's leave that one out, using the first letter of each color as the variable names.
 
-// 6. But wait! Indigo is now feeling *super* left out. Let's only assign indigo using indg. 
 
-// Objects
+const [blackAndWhite, , black, pink] = farmAnimals.split(' ');
+console.log(blackAndWhite, black, pink);
 
-// 7. Use destructuring to assign all variables using the keys as the variable names
 
-// 8. Use destructuring to assign songs 2 and 4, and Kermit's job and partner
+
+
+const [r, o, y, g, b, indg, v] = colors;
+console.log(r, o, y, g, b, indg, v);
+
+
+const [red, orange, yellow, green, blue, indigo, violet] = colors;
+console.log('colors'); 
+
+
+const [r1, o1, y1, g1, b1, , v1] = colors;
+console.log(r1, o1, y1, g1, b1, v1);
+
+
+const [, , , , , indg1] = colors;
+console.log(indg1);
+
+
+
+const { muppetName, color, song, job, partner } = muppet;
+console.log(muppetName, color, song, job, partner);
+
+const {
+  album: {
+    theMuppetMovie: { song2, song4 }
+  },
+  nestedJob,
+  nestedPartner
+} = nestedMuppet;
+
+console.log(song2, song4, nestedJob, nestedPartner);
